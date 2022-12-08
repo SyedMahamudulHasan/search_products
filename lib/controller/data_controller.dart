09129ce32e98ee7cf10e4a0bc99b3ecb80fc5fb1
SyedMahamudulHasan <syedhasan.cse@gmail.com>
2022-12-08 23:02:22 +0600

    created data controller class

---
+++ lib/controller/data_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class DataController extends ChangeNotifier{
+  Future<void> getProducts(String productName, String offset) async {
+    
+  }
+
+}
