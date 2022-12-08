5b4d4df4d8c5a900da34f5cb0c3e23d87dc0f39a
SyedMahamudulHasan <syedhasan.cse@gmail.com>
2022-12-08 21:15:06 +0600

    created src ctr with scrn padding

--- lib/view/search_screen.dart
+++ lib/view/search_screen.dart
@@ -10,8 +10,42 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController? _searchController;
+  @override
+  void initState() {
+    _searchController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: size.height * 0.08,
+              width: size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.white,
+              ),
+              child: TextFormField(
+                controller: _searchController,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
